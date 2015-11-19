8a012d51993c9d41b99edaa1ce70f1476bce173a
unknown <mmar12@KS4M41134.nt.impel.sa>
2015-11-19 09:02:58 +0100

    dostosowanie aplikacji do upublicznienia

--- webservice/src/main/java/com/webservice/peglefiles/models/answear/StationsListResponse.java
+++
@@ -1,7 +0,0 @@
-package com.webservice.peglefiles.models.answear;
-
-/**
- * Created by mmar12 on 2015-10-21.
- */
-public class StationsListResponse {
-}
--- webservice/src/main/run.bat
+++
@@ -1,1 +0,0 @@
-java -cp "Pegle-WebService-0.1.jar" org.springframework.boot.loader.JarLauncher
